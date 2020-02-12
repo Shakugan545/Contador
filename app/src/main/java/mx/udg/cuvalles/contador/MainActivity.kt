8decc4e304224dab341045ce93e39036afa0cd2b
Antonio Nava <tonnymayoral18@hotmail.com>
2020-02-12 08:53:33 -0600

    Hola mundo

--- app/src/main/java/mx/udg/cuvalles/contador/MainActivity.kt
+++ app/src/main/java/mx/udg/cuvalles/contador/MainActivity.kt
@@ -2,21 +2,30 @@ package mx.udg.cuvalles.contador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var contador = 0
-        etiquetaMostrar.text = "$contador"
 
-        btnIncrementar.setOnClickListener {
-            contador ++
-            etiquetaMostrar.text = "$contador"
+
+
+        btnIncrementar.setOnClickListener(this)
+        btndecrementar.setOnClickListener(this)
+    }
+    /*Estamos utilizando una interfaz para capturar todos los clicks*/
+    /*!! para decir que v existe y no es nulo */
+    override fun onClick(v: View?) {
+        if (v!!.id == R.id.btnIncrementar) {
+            contador++
+        } else if (v!!.id == R.id.btndecrementar) {
+            contador--
         }
-
+        etiquetaMostrar.text = "$contador"
     }
 }
